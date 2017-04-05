5471bbfeccbe9e73c52bc79f92bab8f2b51bf381
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-04-05 10:54:18 +0200

    Update CI configuration

--- travis.sh
+++ travis.sh
@@ -127,7 +127,7 @@ BUILD)
 
     mvn clean package jacoco:report sonar:sonar \
           $MAVEN_ARGS \
-          -Pjacoco -Djacoco.includes=org.ligoj.app.plugin.mail.* \
+          -Pjacoco -Djacoco.includes=org.ligoj.app.plugin.mail.*  \
           -Dsonar.host.url=$SONAR_HOST_URL \
           -Dsonar.organization=ligoj-github \
           -Dsonar.login=$SONAR_TOKEN \
