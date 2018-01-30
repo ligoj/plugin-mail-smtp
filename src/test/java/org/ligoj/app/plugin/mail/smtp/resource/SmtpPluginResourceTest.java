bee2128cfab6f5d8b5bcb654e7e31371d3d873ec
Fabrice Daugan <fabrice.daugan@gfi.fr>
2018-01-30 07:59:58 +0100

    Update API to 2.1.0

--- src/test/java/org/ligoj/app/plugin/mail/smtp/resource/SmtpPluginResourceTest.java
+++ src/test/java/org/ligoj/app/plugin/mail/smtp/resource/SmtpPluginResourceTest.java
@@ -6,11 +6,11 @@ import java.util.Collections;
 import javax.transaction.Transactional;
 
 import org.apache.commons.lang3.NotImplementedException;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.ligoj.app.AbstractAppTest;
 import org.ligoj.app.model.Node;
 import org.ligoj.app.model.Parameter;
@@ -22,14 +22,14 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import net.sf.ehcache.CacheManager;
 
 /**
  * Test of {@link SmtpPluginResource}
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:/META-INF/spring/application-context-test.xml")
 @Rollback
 @Transactional
@@ -38,30 +38,30 @@ public class SmtpPluginResourceTest extends AbstractAppTest {
 	@Autowired
 	private SmtpPluginResource resource;
 
-	@Before
+	@BeforeEach
 	public void prepareConfiguration() throws IOException {
 		persistEntities("csv", Node.class, Parameter.class, ParameterValue.class);
 	}
 
-	@Before
-	@After
+	@BeforeEach
+	@AfterEach
 	public void cleanCache() {
 		CacheManager.getInstance().getCache("plugin-data").removeAll();
 	}
 
 	@Test
 	public void getKey() {
-		Assert.assertEquals("service:mail:smtp", resource.getKey());
+		Assertions.assertEquals("service:mail:smtp", resource.getKey());
 	}
 
 	@Test
 	public void getMailSender() {
 		final JavaMailSenderImpl mailSender = (JavaMailSenderImpl) resource.getMailSender("service:mail:smtp:local");
-		Assert.assertEquals("localhost", mailSender.getHost());
-		Assert.assertEquals("user", mailSender.getUsername());
-		Assert.assertEquals("secret", mailSender.getPassword());
-		Assert.assertEquals(25, mailSender.getPort());
-		Assert.assertEquals("UTF-8", mailSender.getDefaultEncoding());
+		Assertions.assertEquals("localhost", mailSender.getHost());
+		Assertions.assertEquals("user", mailSender.getUsername());
+		Assertions.assertEquals("secret", mailSender.getPassword());
+		Assertions.assertEquals(25, mailSender.getPort());
+		Assertions.assertEquals("UTF-8", mailSender.getDefaultEncoding());
 	}
 
 	@Test
@@ -70,36 +70,37 @@ public class SmtpPluginResourceTest extends AbstractAppTest {
 		applicationContext.getAutowireCapableBeanFactory().autowireBean(resource);
 		resource.applicationContext = Mockito.mock(ApplicationContext.class);
 		final SmtpPluginResource mockPLugin = Mockito.mock(SmtpPluginResource.class);
-		Mockito.when(mockPLugin.getMailSender("service:mail:smtp:local"))
-				.thenReturn(Mockito.mock(JavaMailSenderImpl.class));
+		Mockito.when(mockPLugin.getMailSender("service:mail:smtp:local")).thenReturn(Mockito.mock(JavaMailSenderImpl.class));
 		Mockito.when(resource.applicationContext.getBean(SmtpPluginResource.class)).thenReturn(mockPLugin);
 		final MimeMessagePreparator messagePreparator = Mockito.mock(MimeMessagePreparator.class);
-		Assert.assertEquals(messagePreparator, resource.send("service:mail:smtp:local", messagePreparator));
+		Assertions.assertEquals(messagePreparator, resource.send("service:mail:smtp:local", messagePreparator));
 	}
 
-	@Test(expected = NotImplementedException.class)
+	@Test
 	public void link() {
-		resource.link(0);
+		Assertions.assertThrows(NotImplementedException.class, () -> {
+			resource.link(0);
+		});
 	}
 
 	@Test
 	public void getVersion() throws Exception {
-		Assert.assertNull(resource.getVersion(Collections.emptyMap()));
+		Assertions.assertNull(resource.getVersion(Collections.emptyMap()));
 	}
 
 	@Test
 	public void getLastVersion() throws Exception {
-		Assert.assertNull(resource.getLastVersion());
+		Assertions.assertNull(resource.getLastVersion());
 	}
 
 	@Test
 	public void checkStatus() throws Exception {
-		Assert.assertTrue(resource.checkStatus(null, null));
+		Assertions.assertTrue(resource.checkStatus(null, null));
 	}
 
 	@Test
 	public void checkSubscriptionStatus() throws Exception {
-		Assert.assertNull(resource.checkSubscriptionStatus(null, null));
+		Assertions.assertNull(resource.checkSubscriptionStatus(null, null));
 	}
 
 }
