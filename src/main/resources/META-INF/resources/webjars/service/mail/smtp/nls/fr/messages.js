e5011dd5b92ce537dcd6b4569a2c113944c0b3d0
Fabrice Daugan <fabrice.daugan@gmail.com>
2018-02-13 09:43:04 +0100

    Make possible to configure SMTP mail node thought UI

---
+++ src/main/resources/META-INF/resources/webjars/service/mail/smtp/nls/fr/messages.js
@@ -0,0 +1,7 @@
+define({
+	'service:mail:smtp': 'SMTP',
+	'service:mail:smtp:host': 'Hôte',
+	'service:mail:smtp:password': 'Mot de passe',
+	'service:mail:smtp:port': 'Port',
+	'service:mail:smtp:username': 'Utilisateur'
+});
---
+++ src/main/resources/META-INF/resources/webjars/service/mail/smtp/nls/messages.js
@@ -0,0 +1,10 @@
+define({
+	'root': {
+		'service:mail:smtp': 'SMTP',
+		'service:mail:smtp:host': 'Host',
+		'service:mail:smtp:password': 'Password',
+		'service:mail:smtp:port': 'Port',
+		'service:mail:smtp:username': 'Username'
+	},
+	'fr': true
+});
